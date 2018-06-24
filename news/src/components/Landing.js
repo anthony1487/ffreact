87b7163ac87cfa0f801dd981cb9e1459c659fb7b
= <=>
2018-06-24 02:19:33 -0400

    allows for working buttons, ticker, and main title to work with guardian API

---
+++ news/src/components/Landing.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+// import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Ticker from './Ticker';
+import Upper from './Upper';
+import Nav from './Nav';
+import axios from 'axios';
+
+ class Landing extends Component {
+    constructor(){
+        super(); 
+        this.state = {
+            people: [], 
+            breaking: []
+        }
+    
+    }
+
+
+   
+    componentWillMount() {
+        axios.get('https://content.guardianapis.com/search?&api-key=c78d3ee2-a1b2-43f4-9d66-ac228f6b6a0d').then(
+           response => this.setState({ people: [response.data.response.results[7].webTitle,response.data.response.results[9].webTitle,response.data.response.results[5].webTitle]})
+       );
+       axios.get('https://content.guardianapis.com/search?q=breaking&api-key=c78d3ee2-a1b2-43f4-9d66-ac228f6b6a0d').then(
+           response => this.setState({ breaking: [response.data.response.results[0].webTitle +"         ",response.data.response.results[1].webTitle+"          ",response.data.response.results[2].webTitle +"         "]})
+       );
+    
+    }
+
+  
+  
+  
+    render() {
+    return (
+        <div>
+          
+             <Header type="Home" />
+              <Ticker breaking={this.state.breaking} />
+              <Upper title={this.state.people} />
+              <Ticker breaking={this.state.breaking} />
+              <Nav /> 
+             
+               {/* **WILL CALL CARDS (FULL OF CARD) */}
+
+        </div>
+        
+    );
+  }
+}
+
+export default Landing;
--- news/src/components/Main.js
+++ news/src/components/Main.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Ticker from './Ticker';
-import Upper from './Upper';
-import Nav from './Nav';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import Header from './Header';
+// import Ticker from './Ticker';
+// import Upper from './Upper';
+// import Nav from './Nav';
+
+import Landing from './Landing';
+import Cat from './Cat';
+
 class Main extends Component {
   render() {
     return (
-      <div>
-          <Header />
-          <Ticker />
-          <Upper />
-          <Nav />
-          <Header />
-          <Header />
-          <Header />
-          <Header />
-          {/* 
-          <Lower />
-          <Footer /> */}
-      </div>
+        <BrowserRouter>
+            <div>
+               <Route exact path="/" component={ Landing } />
+               <Route  path="/:catId" component={ Cat } />
+          </div>
+     </BrowserRouter>
     );
   }
 }
 
 export default Main;
+
