eca2dc1d04c934d116cc5dedb56c4a33dbeebb83
Anthony Clemow <anthony1487@gmail.com>
2018-06-26 17:21:25 -0400

    fixed cards sizing, added footer

--- news/src/components/Landing.js
+++ news/src/components/Landing.js
@@ -4,22 +4,23 @@ import Header from './Header';
 import Ticker from './Ticker';
 import Upper from './Upper';
 import Nav from './Nav';
+import Footer from './Footer';
 // import Lower from './Lower';
 import axios from 'axios';
 import Buffer from './Buffer';
 
  class Landing extends Component {
     constructor(){
-        super(); 
+        super();
         this.state = {
-            people: [], 
+            people: [],
             breaking: []
         }
-    
+
     }
 
 
-   
+
     componentWillMount() {
         axios.get('https://content.guardianapis.com/search?&api-key=c78d3ee2-a1b2-43f4-9d66-ac228f6b6a0d').then(
            response => this.setState({ people: [response.data.response.results[7].webTitle,response.data.response.results[9].webTitle,response.data.response.results[5].webTitle]})
@@ -27,16 +28,16 @@ import Buffer from './Buffer';
        axios.get('https://content.guardianapis.com/search?q=breaking&api-key=c78d3ee2-a1b2-43f4-9d66-ac228f6b6a0d').then(
            response => this.setState({ breaking: [response.data.response.results[0].webTitle +"         ",response.data.response.results[1].webTitle+"          ",response.data.response.results[2].webTitle +"         "]})
        );
-    
+
     }
 
-  
-  
-  
+
+
+
     render() {
     return (
         <div>
-          
+
              <Header type="News" />
               <Ticker breaking={this.state.breaking} />
               <Upper title={this.state.people} />
@@ -44,13 +45,14 @@ import Buffer from './Buffer';
 
               <Nav />
               <Buffer />
+              <Footer />
               {/* <Lower /> */}
-              
+
                {/* **WILL CALL CARDS (FULL OF CARD) */}
 
 
         </div>
-        
+
     );
   }
 }
--- news/src/components/News/NewSingle.js
+++ news/src/components/News/NewSingle.js
@@ -3,13 +3,13 @@ import "../css/s_newsingle.css";
 
 const NewSingle = ({item}) => (
     <div className="single col s4">
-     <div className="card">
+     <div className="card small">
      <div className="card-image" >
      <img src={item.urlToImage} alt={item.title}/>
      <span className="card-title">{item.source.name}</span>
      </div>
      <div className="card-content">
-     <p>{item.title}</p> 
+     <p>{item.title}</p>
      </div>
      <div className="card-action">
       <a href={item.url} target="_blank">Full article</a>
